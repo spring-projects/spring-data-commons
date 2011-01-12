2f11dd80fd8c5980e0bd7da4e8c6de2eb817e440
Michael Hunger <github@jexp.de>
2011-01-12 10:51:03 +0100

    renamed Index.name to Index.indexName to avoid confusion with potential name property

--- spring-data-commons-core/src/main/java/org/springframework/data/annotation/Indexed.java
+++ spring-data-commons-core/src/main/java/org/springframework/data/annotation/Indexed.java
@@ -5,9 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotated fields and entities will be indexed and available for retrieval using an indexing API.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target( value = {ElementType.FIELD,ElementType.TYPE})
 public @interface Indexed
 {
-    String name() default "";
+    /**
+     * Name of the index to use.
+     */
+    String indexName() default "";
 }
