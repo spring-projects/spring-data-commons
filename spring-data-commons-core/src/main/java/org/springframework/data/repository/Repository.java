0ea1921a962d503ac8f9f65bc0f1fe41061cb69d
Oliver Gierke <info@olivergierke.de>
2011-05-24 09:53:48 +0200

    Updated reference documentation regarding repository declaration.
    Polished Javadoc of Repository to mention possibility of selectively exposing CRUD methods.

--- spring-data-commons-core/src/main/java/org/springframework/data/repository/Repository.java
+++ spring-data-commons-core/src/main/java/org/springframework/data/repository/Repository.java
@@ -3,7 +3,16 @@ package org.springframework.data.repository;
 import java.io.Serializable;
 
 /**
- *
+ * Central repository marker interface. Captures the domain type to manage as well as the domain type's id type. General
+ * purpose is to hold type information as well as being able to discover interfaces that extend this one during
+ * classpath scanning for easy Spring bean creation.
+ * <p>
+ * Domain repositories extending this interface can selectively expose CRUD methods by simply declaring methods of the
+ * same signature as those declared in {@link CrudRepository}.
+ * 
+ * @see CrudRepository
+ * @param <T> the domain type the repository manages
+ * @param <ID> the type of the id of the entity the repository manages
  * @author Oliver Gierke
  */
 public interface Repository<T, ID extends Serializable> {
