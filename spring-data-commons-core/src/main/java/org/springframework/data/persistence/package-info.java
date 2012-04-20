06dc1e5dba4108ad16ebdd04923d21231fa1e037
Oliver Gierke <info@olivergierke.de>
2012-04-20 13:20:14 +0200

    DATACMNS-156 - Removed deprecated persistence package.

--- spring-data-commons-core/src/main/java/org/springframework/data/persistence/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Deprecated entity instantiation API.
- * 
- * @deprecated Use entity instantation API around {@link org.springframework.data.convert.EntityInstantiator} instead.
- */
-package org.springframework.data.persistence;
