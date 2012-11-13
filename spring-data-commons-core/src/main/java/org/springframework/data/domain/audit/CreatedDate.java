853410c5c0abc89b0c441e0f25443a51ad09526c
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2012-11-13 10:22:36 +0800

    DATACMNS-137, DATAJPA-116 - Separate the Auditable into two separate responsibilities: AuditableDate, AuditableBy.
    Added annotation types CreatedBy, CreatedDate, LastModifiedBy, and LastModifiedDate as proposed in https://jira.springsource.org/browse/DATAJPA-116. Added AuditableMetadata for providing a way of discovering auditable classes. Provided unit tests for the discovery process.

---
+++ spring-data-commons-core/src/main/java/org/springframework/data/domain/audit/CreatedDate.java
@@ -0,0 +1,14 @@
+package org.springframework.data.domain.audit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Ranie Jade Ramiso
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.FIELD })
+public @interface CreatedDate {
+}
