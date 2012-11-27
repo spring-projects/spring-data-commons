fe8e48455ec50fca0e460283cdab804b54be35fd
Oliver Gierke <ogierke@vmware.com>
2012-11-27 13:39:36 +0100

    DATACMNS-253 - Improved exception message in ParameterOutOfBoundsException.

--- spring-data-commons-core/src/main/java/org/springframework/data/repository/query/Parameters.java
+++ spring-data-commons-core/src/main/java/org/springframework/data/repository/query/Parameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2011 the original author or authors.
+ * Copyright 2008-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -165,7 +165,8 @@ public class Parameters implements Iterable<Parameter> {
 		try {
 			return parameters.get(index);
 		} catch (IndexOutOfBoundsException e) {
-			throw new ParameterOutOfBoundsException(e);
+			throw new ParameterOutOfBoundsException(
+					"Invalid parameter index! You seem to have declare too little query method parameters!", e);
 		}
 	}
 
