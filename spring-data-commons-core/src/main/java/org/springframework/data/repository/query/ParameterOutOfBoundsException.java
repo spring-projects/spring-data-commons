a05a6d5e7737eed81d53327b5f31daf5c20c6140
Oliver Gierke <ogierke@vmware.com>
2012-11-27 13:39:36 +0100

    DATACMNS-253 - Improved exception message in ParameterOutOfBoundsException.

--- spring-data-commons-core/src/main/java/org/springframework/data/repository/query/ParameterOutOfBoundsException.java
+++ spring-data-commons-core/src/main/java/org/springframework/data/repository/query/ParameterOutOfBoundsException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2010 the original author or authors.
+ * Copyright 2008-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,10 +28,10 @@ public class ParameterOutOfBoundsException extends RuntimeException {
 	/**
 	 * Creates a new {@link ParameterOutOfBoundsException} with the given exception as cause.
 	 * 
+	 * @param message
 	 * @param cause
 	 */
-	public ParameterOutOfBoundsException(Throwable cause) {
-
-		super(cause);
+	public ParameterOutOfBoundsException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
