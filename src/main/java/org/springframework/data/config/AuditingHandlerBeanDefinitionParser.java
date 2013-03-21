9781f603ea1b30a67acde2ee12069dc3a46889b2
Oliver Gierke <info@olivergierke.de>
2013-03-21 15:37:37 +0100

    DATACMNS-295 - Expose modify-on-creation flag in XML namespace.
    Introduced additional attribute in schema in version 1.6. Added parsing of the attribute.

--- src/main/java/org/springframework/data/config/AuditingHandlerBeanDefinitionParser.java
+++ src/main/java/org/springframework/data/config/AuditingHandlerBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,7 @@ public class AuditingHandlerBeanDefinitionParser extends AbstractSingleBeanDefin
 
 		ParsingUtils.setPropertyValue(builder, element, "set-dates", "dateTimeForNow");
 		ParsingUtils.setPropertyReference(builder, element, "date-time-provider-ref", "dateTimeProvider");
-
+		ParsingUtils.setPropertyValue(builder, element, "modify-on-creation", "modifyOnCreation");
 	}
 
 	private BeanDefinition createLazyInitTargetSourceBeanDefinition(String auditorAwareRef) {
