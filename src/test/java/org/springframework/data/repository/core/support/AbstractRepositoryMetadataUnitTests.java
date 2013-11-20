b96f613b98cccad6bb7fa84950dd5178931c35aa
Thomas Darimont <tdarimont@gopivotal.com>
2013-11-20 15:25:37 +0100

    DATACMNS-406 - Tighten contract for RepositoryMetadata implementations.
    We now resolve the domain and id-types eagerly within the constructor of AbstractRepositoryMetadata implementations to prevent a RepositoryMetadata instance to be created in an invalid state. Pulled-up repositoryInterface property to AbstractRepositoryMetadata.
    
    Original pull request: #58.

--- src/test/java/org/springframework/data/repository/core/support/AbstractRepositoryMetadataUnitTests.java
+++ src/test/java/org/springframework/data/repository/core/support/AbstractRepositoryMetadataUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012 the original author or authors.
+ * Copyright 2011-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ import org.springframework.data.repository.core.RepositoryMetadata;
  * Unit tests for {@link AbstractRepositoryMetadata}.
  * 
  * @author Oliver Gierke
+ * @author Thomas Darimont
  */
 public class AbstractRepositoryMetadataUnitTests {
 
@@ -116,10 +117,6 @@ public class AbstractRepositoryMetadataUnitTests {
 		}
 
 		public Class<?> getDomainType() {
-			return null;
-		}
-
-		public Class<?> getRepositoryInterface() {
 			return null;
 		}
 	}
