f148d19c81786a30bc9f167bee28c3d2090413c5
Thomas Darimont <tdarimont@gopivotal.com>
2013-12-13 12:24:38 +0100

    DATACMNS-410 - Fixed bug in ReflectionRepositoryInvoker#invokeFindOne.
    We now delegate calls to invokeFindOne to the correct target repository.
    
    Original pull request: #80.

--- src/test/java/org/springframework/data/repository/support/ReflectionRepositoryInvokerUnitTests.java
+++ src/test/java/org/springframework/data/repository/support/ReflectionRepositoryInvokerUnitTests.java
@@ -23,7 +23,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.core.CrudInvoker;
 import org.springframework.data.repository.core.CrudMethods;
 
 /**
@@ -34,7 +35,7 @@ import org.springframework.data.repository.core.CrudMethods;
 @RunWith(MockitoJUnitRunner.class)
 public class ReflectionRepositoryInvokerUnitTests {
 
-	@Mock Repository<Object, Serializable> repo;
+	@Mock CrudRepository<Object, Serializable> repo;
 	@Mock CrudMethods methods;
 
 	@Test
@@ -71,5 +72,38 @@ public class ReflectionRepositoryInvokerUnitTests {
 		when(methods.hasSaveMethod()).thenReturn(false);
 
 		new ReflectionRepositoryInvoker<Object>(repo, methods);
+	}
+
+	/**
+	 * @see DATACMNS-410
+	 */
+	@Test
+	public void invokesFindOneCorrectly() throws Exception {
+
+		when(methods.hasFindOneMethod()).thenReturn(true);
+		when(methods.hasSaveMethod()).thenReturn(true);
+		when(methods.getFindOneMethod()).thenReturn(CrudRepository.class.getMethod("findOne", Serializable.class));
+
+		CrudInvoker<Object> invoker = new ReflectionRepositoryInvoker<Object>(repo, methods);
+		invoker.invokeFindOne(1L);
+
+		verify(repo, times(1)).findOne(1L);
+	}
+
+	/**
+	 * @see DATACMNS-410
+	 */
+	@Test
+	public void invokesSaveCorrectly() throws Exception {
+
+		when(methods.hasFindOneMethod()).thenReturn(true);
+		when(methods.hasSaveMethod()).thenReturn(true);
+		when(methods.getSaveMethod()).thenReturn(CrudRepository.class.getMethod("save", Object.class));
+
+		CrudInvoker<Object> invoker = new ReflectionRepositoryInvoker<Object>(repo, methods);
+		Object object = new Object();
+		invoker.invokeSave(object);
+
+		verify(repo, times(1)).save(object);
 	}
 }
