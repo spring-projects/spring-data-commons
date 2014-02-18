9ca70e3eb5c61189d35526835190ab439d19783e
Thomas Darimont <tdarimont@gopivotal.com>
2014-02-18 14:55:50 +0100

    DATACMNS-441 - Analyzing @Query annotated bridge methods lead to NPEs during QueryMethod processing in Java 8.
    We now skip bridge methods for @Query method processing in order to circumvent the changed behaviour in Class#getMethods() in Java 8 (when the code was compiled with target byte code level 1.8) which results in bridge methods being found and inspected for query method annotations. Since we currently cannot analyse generic return types of bridge methods this results in NPEs being thrown at a later stage in AbstractRepositoryMetadata#getReturnedDomainClass(..).
    See also DATAJPA-465.
    
    Original pull request: #64

--- src/main/java/org/springframework/data/repository/core/support/DefaultRepositoryInformation.java
+++ src/main/java/org/springframework/data/repository/core/support/DefaultRepositoryInformation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 the original author or authors.
+ * Copyright 2011-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,7 +172,8 @@ class DefaultRepositoryInformation implements RepositoryInformation {
 	 * @return
 	 */
 	private boolean isQueryMethodCandidate(Method method) {
-		return isQueryAnnotationPresentOn(method) || !isCustomMethod(method) && !isBaseClassMethod(method);
+		return !method.isBridge() //
+				&& (isQueryAnnotationPresentOn(method) || !isCustomMethod(method) && !isBaseClassMethod(method));
 	}
 
 	/**
