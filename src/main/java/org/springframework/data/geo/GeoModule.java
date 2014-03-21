176be042c9dee608fbc15defd72af042b4f507ae
Oliver Gierke <ogierke@gopivotal.com>
2014-03-21 13:20:54 +0100

    DATACMNS-475 - Added Jackson 2 module to (de)serialize new geo types.
    Newly introduced GeoModule contains mixins for Distance, Point, Circle, Box and Polygon.

---
+++ src/main/java/org/springframework/data/geo/GeoModule.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.geo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+/**
+ * Jackson 2 Module to deserialize
+ * 
+ * @author Oliver Gierke
+ * @since 1.8
+ */
+public class GeoModule extends SimpleModule {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new {@link GeoModule} registering mixins for common geo-spatial types.
+	 */
+	public GeoModule() {
+
+		super("Spring Data Geo Mixins", new Version(1, 0, 0, null, "org.springframework.data", "spring-data-commons-geo"));
+
+		setMixInAnnotation(Distance.class, DistanceMixin.class);
+		setMixInAnnotation(Point.class, PointMixin.class);
+		setMixInAnnotation(Box.class, BoxMixin.class);
+		setMixInAnnotation(Circle.class, CircleMixin.class);
+		setMixInAnnotation(Polygon.class, PolygonMixin.class);
+	}
+
+	static abstract class DistanceMixin {
+
+		DistanceMixin(@JsonProperty("value") double value,
+				@JsonProperty("metric") @JsonDeserialize(as = Metrics.class) Metric metic) {}
+
+		@JsonIgnore
+		abstract double getNormalizedValue();
+	}
+
+	static abstract class PointMixin {
+		PointMixin(@JsonProperty("x") double x, @JsonProperty("y") double y) {}
+	}
+
+	static abstract class CircleMixin {
+		CircleMixin(@JsonProperty("center") Point center, @JsonProperty("radius") Distance radius) {}
+	}
+
+	static abstract class BoxMixin {
+		BoxMixin(@JsonProperty("first") Point first, @JsonProperty("second") Point point) {}
+	}
+
+	static abstract class PolygonMixin {
+		PolygonMixin(@JsonProperty("points") List<Point> points) {}
+	}
+}
---
+++ src/test/java/org/springframework/data/geo/GeoModuleIntegrationTests.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.geo;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Integration tests for {@link GeoModule}.
+ * 
+ * @author Oliver Gierke
+ */
+public class GeoModuleIntegrationTests {
+
+	ObjectMapper mapper;
+
+	@Before
+	public void setUp() {
+
+		this.mapper = new ObjectMapper();
+		this.mapper.registerModule(new GeoModule());
+	}
+
+	/**
+	 * @see DATACMNS-475
+	 */
+	@Test
+	public void deserializesDistance() throws Exception {
+
+		String json = "{\"value\":10.0,\"metric\":\"KILOMETERS\"}";
+		Distance reference = new Distance(10.0, Metrics.KILOMETERS);
+
+		assertThat(mapper.readValue(json, Distance.class), is(reference));
+		assertThat(mapper.writeValueAsString(reference), is(json));
+	}
+
+	/**
+	 * @see DATACMNS-475
+	 */
+	@Test
+	public void deserializesPoint() throws Exception {
+
+		String json = "{\"x\":10.0,\"y\":20.0}";
+		Point reference = new Point(10.0, 20.0);
+
+		assertThat(mapper.readValue(json, Point.class), is(reference));
+		assertThat(mapper.writeValueAsString(reference), is(json));
+	}
+
+	/**
+	 * @see DATACMNS-475
+	 */
+	@Test
+	public void deserializesCircle() throws Exception {
+
+		String json = "{\"center\":{\"x\":10.0,\"y\":20.0},\"radius\":{\"value\":10.0,\"metric\":\"KILOMETERS\"}}";
+		Circle reference = new Circle(new Point(10.0, 20.0), new Distance(10, Metrics.KILOMETERS));
+
+		assertThat(mapper.readValue(json, Circle.class), is(reference));
+		assertThat(mapper.writeValueAsString(reference), is(json));
+	}
+
+	/**
+	 * @see DATACMNS-475
+	 */
+	@Test
+	public void deserializesBox() throws Exception {
+
+		String json = "{\"first\":{\"x\":1.0,\"y\":2.0},\"second\":{\"x\":2.0,\"y\":3.0}}";
+		Box reference = new Box(new Point(1, 2), new Point(2, 3));
+
+		assertThat(mapper.readValue(json, Box.class), is(reference));
+		assertThat(mapper.writeValueAsString(reference), is(json));
+	}
+
+	/**
+	 * @see DATACMNS-475
+	 */
+	@Test
+	public void deserializesPolygon() throws Exception {
+
+		String json = "{\"points\":[{\"x\":1.0,\"y\":2.0},{\"x\":2.0,\"y\":3.0},{\"x\":3.0,\"y\":4.0}]}";
+		Polygon reference = new Polygon(new Point(1, 2), new Point(2, 3), new Point(3, 4));
+
+		assertThat(mapper.readValue(json, Polygon.class), is(reference));
+		assertThat(mapper.writeValueAsString(reference), is(json));
+	}
+}
