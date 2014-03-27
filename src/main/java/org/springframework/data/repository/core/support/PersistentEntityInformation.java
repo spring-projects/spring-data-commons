9bd8e4db81be38dac4ed03a4577309e75f29aab1
Oliver Gierke <ogierke@gopivotal.com>
2014-03-27 18:40:58 +0100

    DATACMNS-480 - Added EntityInformation implementation based on PersistentEntity.
    Related ticket: DATACMNS-476.

---
+++ src/main/java/org/springframework/data/repository/core/support/PersistentEntityInformation.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.repository.core.support;
+
+import java.io.Serializable;
+
+import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.mapping.model.BeanWrapper;
+import org.springframework.data.repository.core.EntityInformation;
+
+/**
+ * {@link EntityInformation} implementation that uses a {@link PersistentEntity} to obtain id type information and uses
+ * a {@link BeanWrapper} to access the property value if requested.
+ * 
+ * @author Oliver Gierke
+ */
+@SuppressWarnings("unchecked")
+public class PersistentEntityInformation<T, ID extends Serializable> extends AbstractEntityInformation<T, ID> {
+
+	private final PersistentEntity<T, ?> persistentEntity;
+
+	/**
+	 * Creates a new {@link PersistableEntityInformation} for the given {@link PersistentEntity}.
+	 * 
+	 * @param entity must not be {@literal null}.
+	 */
+	public PersistentEntityInformation(PersistentEntity<T, ?> entity) {
+
+		super(entity.getType());
+		this.persistentEntity = entity;
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.core.EntityInformation#getId(java.lang.Object)
+	 */
+	@Override
+	public ID getId(T entity) {
+		return (ID) BeanWrapper.create(entity, null).getProperty(this.persistentEntity.getIdProperty());
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.core.EntityInformation#getIdType()
+	 */
+	@Override
+	public Class<ID> getIdType() {
+		return (Class<ID>) persistentEntity.getIdProperty().getType();
+	}
+}
---
+++ src/test/java/org/springframework/data/repository/core/support/PersistentEntityInformationUnitTests.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.repository.core.support;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.mapping.context.SampleMappingContext;
+import org.springframework.data.mapping.context.SamplePersistentProperty;
+import org.springframework.data.repository.core.EntityInformation;
+
+/**
+ * Unit tests for {@link PersistentEntityInformation}.
+ * 
+ * @author Oliver Gierke
+ */
+public class PersistentEntityInformationUnitTests {
+
+	/**
+	 * @see DATACMNS-480
+	 */
+	@Test
+	public void obtainsIdAndIdTypeInformationFromPersistentEntity() {
+
+		SampleMappingContext context = new SampleMappingContext();
+		PersistentEntity<Object, SamplePersistentProperty> entity = context.getPersistentEntity(Sample.class);
+
+		EntityInformation<Object, Long> information = new PersistentEntityInformation<Object, Long>(entity);
+		assertThat(information.getIdType(), is(typeCompatibleWith(Long.class)));
+
+		Sample sample = new Sample();
+		sample.id = 5L;
+
+		assertThat(information.getId(sample), is(5L));
+	}
+
+	static class Sample {
+
+		@Id Long id;
+	}
+}
