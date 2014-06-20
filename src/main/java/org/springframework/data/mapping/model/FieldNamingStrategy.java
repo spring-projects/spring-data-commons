a885b57dbff2b69c20beaed59a6fe20274e62cd5
Oliver Gierke <ogierke@gopivotal.com>
2014-06-20 21:22:21 +0200

    DATACMNS-523 - Pulled up FieldNamingStrategy SPI from Spring Data MongoDB.
    Pulled up the SPI for field naming from Spring Data MongoDB for reuse in other Spring Data modules.

---
+++ src/main/java/org/springframework/data/mapping/model/FieldNamingStrategy.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.mapping.model;
+
+import org.springframework.data.mapping.PersistentProperty;
+
+/**
+ * SPI to determine how to name document fields in cases the field name is not manually defined.
+ * 
+ * @see PropertyNameFieldNamingStrategy
+ * @see CamelCaseAbbreviatingFieldNamingStrategy
+ * @see SnakeCaseFieldNamingStrategy
+ * @since 1.9
+ * @author Oliver Gierke
+ */
+public interface FieldNamingStrategy {
+
+	/**
+	 * Returns the field name to be used for the given {@link MongoPersistentProperty}.
+	 * 
+	 * @param property must not be {@literal null} or empty;
+	 * @return
+	 */
+	String getFieldName(PersistentProperty<?> property);
+}
---
+++ src/test/java/org/springframework/data/mapping/model/CamelCaseAbbreviatingFieldNamingStrategyUnitTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.mapping.model;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.mapping.PersistentProperty;
+
+/**
+ * Unit tests for {@link CamelCaseAbbreviatingFieldNamingStrategy}.
+ * 
+ * @author Oliver Gierke
+ * @since 1.9
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CamelCaseAbbreviatingFieldNamingStrategyUnitTests {
+
+	FieldNamingStrategy strategy = new CamelCaseAbbreviatingFieldNamingStrategy();
+
+	@Mock PersistentProperty<?> property;
+
+	/**
+	 * @see DATACMNS-523
+	 */
+	@Test
+	public void abbreviatesToCamelCase() {
+
+		assertFieldNameForPropertyName("fooBar", "fb");
+		assertFieldNameForPropertyName("fooBARFooBar", "fbfb");
+	}
+
+	private void assertFieldNameForPropertyName(String propertyName, String fieldName) {
+
+		when(property.getName()).thenReturn(propertyName);
+		assertThat(strategy.getFieldName(property), is(fieldName));
+	}
+}
