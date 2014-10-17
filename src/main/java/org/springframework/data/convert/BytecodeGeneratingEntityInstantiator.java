a7477c800575c4ba5e83cc08c9d9eab288ae231f
Thomas Darimont <tdarimont@pivotal.io>
2014-10-17 15:24:34 +0200

    DATACMNS-578 - Speed up instance creation by generating Factory classes with ASM.
    Cleaned up byte code generation and removed usage of JDK 8 specific types.

--- src/main/java/org/springframework/data/convert/BytecodeGeneratingEntityInstantiator.java
+++ src/main/java/org/springframework/data/convert/BytecodeGeneratingEntityInstantiator.java
@@ -19,13 +19,14 @@ import static org.springframework.asm.Opcodes.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.asm.ClassWriter;
-import org.springframework.asm.Label;
 import org.springframework.asm.MethodVisitor;
 import org.springframework.asm.Opcodes;
 import org.springframework.asm.Type;
@@ -34,12 +35,15 @@ import org.springframework.data.mapping.PersistentProperty;
 import org.springframework.data.mapping.PreferredConstructor;
 import org.springframework.data.mapping.PreferredConstructor.Parameter;
 import org.springframework.data.mapping.model.ParameterValueProvider;
+import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 /**
- * {@link EntityInstantiator} that uses the {@link PersistentEntity}'s {@link PreferredConstructor} to instantiate an
- * instance of the entity by dynamically generating factory methods via ASM. Since we cannot support every use case with byte
- * code generation we gracefully fall-back to the {@link ReflectionEntityInstantiator}.
+ * An {@link EntityInstantiator} that can generate byte code to speed-up dynamic object instantiation.
+ * <p>
+ * Uses the {@link PersistentEntity}'s {@link PreferredConstructor} to instantiate an instance of the entity by
+ * dynamically generating factory methods with appropriate constructor invocations via ASM. Since we cannot support
+ * every use case with byte code generation we gracefully fall-back to the {@link ReflectionEntityInstantiator}.
  * 
  * @author Thomas Darimont
  * @since 1.10
@@ -48,48 +52,68 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 
 	INSTANCE;
 
-	private final ObjectCreatorClassGenerator classGenerator = ObjectCreatorClassGenerator.INSTANCE;
+	private final ObjectInstantiatorClassGenerator classGenerator = ObjectInstantiatorClassGenerator.INSTANCE;
 
-	private volatile Map<CreatorKey, ObjectCreator> creators = new HashMap<CreatorKey, ObjectCreator>(32);
+	private volatile Map<InstantiatorKey, ObjectInstantiator> creators = new HashMap<InstantiatorKey, ObjectInstantiator>(32);
 
+	private final Object lock = new Object();
+
+	/* (non-Javadoc)
+	 * @see org.springframework.data.convert.EntityInstantiator#createInstance(org.springframework.data.mapping.PersistentEntity, org.springframework.data.mapping.model.ParameterValueProvider)
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T, E extends PersistentEntity<? extends T, P>, P extends PersistentProperty<P>> T createInstance(E entity,
 			ParameterValueProvider<P> provider) {
 
 		Class<? extends T> type = entity.getType();
-
 		if (shouldUseReflectiveEntityInstantiator(entity)) {
 			return ReflectionEntityInstantiator.INSTANCE.createInstance(entity, provider);
 		}
 
-		PreferredConstructor<? extends T, P> ctor = entity.getPersistenceConstructor();
 
-		List<Object> params = new ArrayList<Object>();
-		if (null != provider && ctor.hasParameters()) {
-			for (Parameter<?, P> parameter : ctor.getParameters()) {
+		PreferredConstructor<? extends T, P> prefCtor = entity.getPersistenceConstructor();
+		
+				List<Object> params = new ArrayList<Object>();
+		if (null != provider && prefCtor.hasParameters()) {
+			for (Parameter<?, P> parameter : prefCtor.getParameters()) {
 				params.add(provider.getParameterValue(parameter));
 			}
 		}
 
 		Object[] args = params.toArray();
-		Constructor<?> ctorToUse = ctor != null && Modifier.isPublic(ctor.getConstructor().getModifiers()) ? ctor
-				.getConstructor() : findAppropriateConstructorForArguments(type, args);
+		Constructor<? extends T> ctor = prefCtor.getConstructor();
+
+		Constructor<?> ctorToUse = prefCtor != null && Modifier.isPublic(ctor.getModifiers()) ? prefCtor.getConstructor()
+				: findAppropriateConstructorForArguments(type, args);
 
 		if (ctorToUse == null || !assertConstructorTypesAreCompatibleWithArgTypes(args, ctorToUse)) {
 			return ReflectionEntityInstantiator.INSTANCE.createInstance(entity, provider);
 		}
 
-		return (T) createInternal(entity.getClass(), ctor.getConstructor(), args);
+		return (T) createInternal(entity.getType(), prefCtor.getConstructor(), args);
 	}
 
+	/**
+	 * Ensures that the given {@link Constructor} can be used with the given {@param args}. 
+	 * 
+	 * @param args
+	 * @param ctorToUse
+	 * @return
+	 */
 	private boolean assertConstructorTypesAreCompatibleWithArgTypes(Object[] args, Constructor<?> ctorToUse) {
 
 		for (int i = 0; ctorToUse != null && i < args.length; i++) {
 			Class<?>[] ctorParamTypes = ctorToUse.getParameterTypes();
 
-			// ignore null arguments
 			if (args[i] == null) {
+
+				// the ctor cannot be used since null cannot be boxed to a wrapper
+				if (ctorParamTypes[i].isPrimitive()) {
+					return false;
+				}
+
+				// ignore null arguments otherwise
 				continue;
 			}
 
@@ -102,6 +126,12 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 		return true;
 	}
 
+	/**
+	 * Determines whether we should fall back to the {@link ReflectionEntityInstantiator} for the given {@link PersistentEntity}.
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	private <T, E extends PersistentEntity<? extends T, P>, P extends PersistentProperty<P>> boolean shouldUseReflectiveEntityInstantiator(
 			E entity) {
 
@@ -114,7 +144,6 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 				|| type.isArray() //
 				|| !Modifier.isPublic(type.getModifiers()) //
 				|| (persistenceCtor != null //
-				// && persistenceCtor.getConstructor().getAnnotation(PersistenceConstructor.class) != null //
 				&& !Modifier.isPublic(persistenceCtor.getConstructor().getModifiers())) //
 		;
 	}
@@ -126,9 +155,9 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 
 		ctorLoop: for (Constructor<?> ctorCandidate : clazz.getDeclaredConstructors()) {
 
-			if (ctorCandidate.getParameterCount() == args.length) {
+			Class<?>[] parameterTypes = ctorCandidate.getParameterTypes();
+			if (parameterTypes.length == args.length) {
 
-				Class<?>[] parameterTypes = ctorCandidate.getParameterTypes();
 				for (int i = 0; i < args.length; i++) {
 
 					if (args[i] == null) {
@@ -153,47 +182,69 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 
 	private Object createInternal(Class<?> clazz, Constructor<?> ctor, Object... args) {
 
-		CreatorKey key = new CreatorKey(clazz, ctor);
+		InstantiatorKey key = new InstantiatorKey(clazz, ctor);
 
-		Map<CreatorKey, ObjectCreator> map = this.creators;
-		ObjectCreator creator = map.get(key);
+		Map<InstantiatorKey, ObjectInstantiator> map = this.creators;
+		ObjectInstantiator creator = map.get(key);
 
 		if (creator == null) {
-			creator = createObjectCreator(key);
+			synchronized (lock) {
 
-			map = new HashMap<CreatorKey, ObjectCreator>(map);
-			map.put(key, creator);
+				map = this.creators;
+				creator = map.get(key);
 
-			this.creators = map;
+				if (creator == null) {
+
+					creator = createObjectCreator(key);
+					map = new HashMap<InstantiatorKey, ObjectInstantiator>(map);
+					map.put(key, creator);
+
+					this.creators = map;
+				}
+			}
 		}
-
-		// System.out.println("Use creator to construct new object: " + creator);
 
 		return creator.create(args);
 	}
 
-	private ObjectCreator createObjectCreator(CreatorKey key) {
-
-		// System.out.println("######Generate class: " + key);
+	/**
+	 * Creates a dynamically generated {@link ObjectInstantiator} for the given {@link InstantiatorKey}.
+	 * 
+	 * There will always be exactly one {@link ObjectInstantiator} instance per {@link InstantiatorKey}.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	private ObjectInstantiator createObjectCreator(InstantiatorKey key) {
 
 		try {
-			return (ObjectCreator) classGenerator.generateClass(key).newInstance();
+			return (ObjectInstantiator) classGenerator.generateCustomInstantiatorClass(key).newInstance();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	/**
+	 * A key that forms a unique tuple for a type and constructor combination.<p>
+	 * 
 	 * @author Thomas Darimont
 	 */
-	static class CreatorKey {
+	static class InstantiatorKey {
 
 		private final Class<?> type;
-		private final Constructor<?> ctor;
+		private final Constructor<?> constructor;
 
-		CreatorKey(Class<?> type, Constructor<?> ctor) {
+		InstantiatorKey(Class<?> type, Constructor<?> constructor) {
 			this.type = type;
-			this.ctor = ctor;
+			this.constructor = constructor;
+		}
+
+		public Class<?> getType() {
+			return type;
+		}
+
+		public Constructor<?> getConstructor() {
+			return constructor;
 		}
 
 		@Override
@@ -203,9 +254,9 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 			if (o == null || getClass() != o.getClass())
 				return false;
 
-			CreatorKey that = (CreatorKey) o;
+			InstantiatorKey that = (InstantiatorKey) o;
 
-			if (ctor != null ? !ctor.equals(that.ctor) : that.ctor != null)
+			if (constructor != null ? !constructor.equals(that.constructor) : that.constructor != null)
 				return false;
 			if (!type.equals(that.type))
 				return false;
@@ -216,17 +267,19 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 		@Override
 		public int hashCode() {
 			int result = type.hashCode();
-			result = 31 * result + (ctor != null ? ctor.hashCode() : 0);
+			result = 31 * result + (constructor != null ? constructor.hashCode() : 0);
 			return result;
 		}
 
 		@Override
 		public String toString() {
-			return "CreatorKey [type=" + type + ", ctor=" + ctor + "]";
+			return "CreatorKey [type=" + type + ", constructor=" + constructor + "]";
 		}
 	}
 
 	/**
+	 * A {@link ClassLoader} that can load classes from {@code byte[]} representations.
+	 * 
 	 * @author Thomas Darimont
 	 */
 	static class ByteArrayClassLoader extends ClassLoader {
@@ -235,8 +288,19 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 			super(parent);
 		}
 
+		/**
+		 * Tries to load a class given {@code byte[]}.
+		 * 
+		 * @param name must not be {@literal null}
+		 * @param bytes must not be {@literal null}
+		 * @return
+		 */
 		public Class<?> loadClass(String name, byte[] bytes) {
 
+			Assert.notNull(name, "name must not be null");
+			Assert.notNull(bytes, "bytes must not be null");
+
+			
 			try {
 				Class<?> clazz = findClass(name);
 				if (clazz != null) {
@@ -251,126 +315,150 @@ public enum BytecodeGeneratingEntityInstantiator implements EntityInstantiator {
 	/**
 	 * @author Thomas Darimont
 	 */
-	public static interface ObjectCreator {
+	public static interface ObjectInstantiator {
 
 		Object create(Object... args);
 	}
 
 	/**
-	 * A dynamic factory class generator.
-	 * 
-	 * This code generator will lazily generate a custom factory class implementing the {@link ObjectCreator} interface
-	 * for every publicly accessed constructor variant.
-	 *  
+	 * Generates a new {@link ObjectInstantiator} class for the given custom class.
+	 * <p>
+	 * This code generator will generate a custom factory class implementing the {@link ObjectInstantiator} interface for
+	 * every publicly accessed constructor variant.
+	 * <p>
 	 * Given a class {@code ObjCtor1ParamString} like:
-	 * <pre> {@code
-	 *  public class ObjCtor1ParamString extends ObjCtorNoArgs {
-	 *
-	 *		public final String param1;
-	 *
-	 *		public ObjCtor1ParamString(String param1) {
-	 *			this.param1 = param1;
-	 *		}
-	 *	}}</pre>
+	 * 
+	 * <pre>
+	 * {
+	 * 	&#064;code
+	 * 	public class ObjCtor1ParamString extends ObjCtorNoArgs {
+	 * 
+	 * 		public final String param1;
+	 * 
+	 * 		public ObjCtor1ParamString(String param1) {
+	 * 			this.param1 = param1;
+	 * 		}
+	 * 	}
+	 * }
+	 * </pre>
 	 *
 	 * The following factory class {@code ObjCtor1ParamString_Creator_asdf} is generated:
-	 * <pre> {@code
-	 *  public class ObjCtor1ParamString_Creator_asdf implements ObjectCreator{
-	 *
-	 *		public Object create(Object... args) {
-	 *		   return new ObjCtor1ParamString((String)args[0]);
-	 *		}
-	 *	}}</pre>
+	 * 
+	 * <pre>
+	 * {
+	 * 	&#064;code
+	 * 	public class ObjCtor1ParamString_Creator_asdf implements ObjectCreator {
+	 * 
+	 * 		public Object create(Object... args) {
+	 * 			return new ObjCtor1ParamString((String) args[0]);
+	 * 		}
+	 * 	}
+	 * }
+	 * </pre>
 	 * 
 	 * @author Thomas Darimont
 	 */
-	enum ObjectCreatorClassGenerator {
-		
+	enum ObjectInstantiatorClassGenerator {
+
 		INSTANCE;
 
-		private static final String[] INTERNAL_OBJECT_CREATOR_INTERFACE_NAME = new String[] { Type
-				.getInternalName(ObjectCreator.class) };
+		private static final String INIT = "<init>";
+		private static final String TAG = "_Instantiator_";
+		private static final String JAVA_LANG_OBJECT = "java/lang/Object";
+		private static final String CREATE_METHOD_NAME = "create";
 
-		private final ByteArrayClassLoader classLoader = new ByteArrayClassLoader(
-				ObjectCreatorClassGenerator.class.getClassLoader());
+		private static final String[] IMPLEMENTED_INTERFACES = new String[] { Type
+				.getInternalName(ObjectInstantiator.class) };
+
+		private final ByteArrayClassLoader classLoader;
+
+		private ObjectInstantiatorClassGenerator() {
+
+			this.classLoader = AccessController.doPrivileged(new PrivilegedAction<ByteArrayClassLoader>() {
+				public ByteArrayClassLoader run() {
+					return new ByteArrayClassLoader(ObjectInstantiatorClassGenerator.class.getClassLoader());
+				}
+			});
+		}
 
 		/**
-		 * Generate a new class for the given {@link CreatorKey}.
+		 * Generate a new class for the given {@link InstantiatorKey}.
 		 * 
 		 * @param key
 		 * @return
 		 */
-		public Class<?> generateClass(CreatorKey key) {
+		public Class<?> generateCustomInstantiatorClass(InstantiatorKey key) {
 
-			Constructor<?> ctor = key.ctor;
+			String className = generateClassName(key);
+			byte[] bytecode = generateBytecode(className, key);
 
-			Class<?> objectClass = ctor.getDeclaringClass();
+			return classLoader.loadClass(className, bytecode);
+		}
 
-			String creatorSuffix = "_Creator_" + Integer.toString(key.hashCode(), 36);
-			String creatorClassResourcePath = Type.getInternalName(objectClass) + creatorSuffix;
-			String entityTypeResourcePath = Type.getInternalName(objectClass);
+		/**
+		 * @param key
+		 * @return
+		 */
+		private String generateClassName(InstantiatorKey key) {
+			return key.getType().getName() + TAG + Integer.toString(key.hashCode(), 36);
+		}
+
+		/**
+		 * Generate a new class for the given {@link InstantiatorKey}.
+		 * 
+		 * @param key
+		 * @return
+		 */
+		public byte[] generateBytecode(String internalClassName, InstantiatorKey key) {
 
 			ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-			MethodVisitor mv;
 
-			cw.visit(Opcodes.V1_6, ACC_PUBLIC + ACC_SUPER, creatorClassResourcePath, null, "java/lang/Object",
-					INTERNAL_OBJECT_CREATOR_INTERFACE_NAME);
+			cw.visit(Opcodes.V1_6, ACC_PUBLIC + ACC_SUPER, internalClassName.replace('.', '/'), null, JAVA_LANG_OBJECT,
+					IMPLEMENTED_INTERFACES);
 
-			{
-				mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
-				mv.visitCode();
-				Label l0 = new Label();
-				mv.visitLabel(l0);
+			visitDefaultConstructor(cw);
 
-				mv.visitVarInsn(ALOAD, 0);
-				mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-				mv.visitInsn(RETURN);
-				Label l1 = new Label();
-				mv.visitLabel(l1);
-				mv.visitLocalVariable("this", "L" + creatorClassResourcePath + ";", null, l0, l1, 0);
-				mv.visitMaxs(1, 1);
-				mv.visitEnd();
-			}
-
-			{
-				mv = cw.visitMethod(ACC_PUBLIC + ACC_VARARGS, "create", "([Ljava/lang/Object;)Ljava/lang/Object;", null, null);
-				mv.visitCode();
-				Label l0 = new Label();
-				mv.visitLabel(l0);
-				mv.visitTypeInsn(NEW, entityTypeResourcePath);
-				mv.visitInsn(DUP);
-
-				if (ctor.getParameterCount() == 0) {
-					mv.visitMethodInsn(INVOKESPECIAL, entityTypeResourcePath, "<init>", "()V", false);
-				} else {
-
-					for (int i = 0; i < ctor.getParameterCount(); i++) {
-
-						mv.visitVarInsn(ALOAD, 1);
-						mv.visitIntInsn(BIPUSH, i);
-
-						mv.visitInsn(AALOAD);
-						mv.visitTypeInsn(CHECKCAST, Type.getInternalName(ctor.getParameterTypes()[i]));
-					}
-
-					mv.visitMethodInsn(INVOKESPECIAL, entityTypeResourcePath, "<init>", Type.getConstructorDescriptor(ctor),
-							false);
-				}
-
-				mv.visitInsn(ARETURN);
-
-				Label l1 = new Label();
-				mv.visitLabel(l1);
-				mv.visitLocalVariable("this", "L" + creatorClassResourcePath + ";", null, l0, l1, 0);
-				mv.visitLocalVariable("args", "[Ljava/lang/Object;", null, l0, l1, 1);
-
-				mv.visitMaxs(0, 0); // computed via ClassWriter.COMPUTE_MAXS
-				mv.visitEnd();
-			}
+			visitCreateMethod(cw, key);
 
 			cw.visitEnd();
 
-			return classLoader.loadClass(objectClass.getName() + creatorSuffix, cw.toByteArray());
+			return cw.toByteArray();
+		}
+
+		private void visitDefaultConstructor(ClassWriter cw) {
+
+			MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, INIT, "()V", null, null);
+			mv.visitCode();
+			mv.visitVarInsn(ALOAD, 0);
+			mv.visitMethodInsn(INVOKESPECIAL, JAVA_LANG_OBJECT, INIT, "()V", false);
+			mv.visitInsn(RETURN);
+			mv.visitMaxs(0, 0); // (0, 0) = computed via ClassWriter.COMPUTE_MAXS
+			mv.visitEnd();
+		}
+
+		private void visitCreateMethod(ClassWriter cw, InstantiatorKey key) {
+
+			String objectClassResourcePath = Type.getInternalName(key.getType());
+
+			MethodVisitor mv = cw.visitMethod(ACC_PUBLIC + ACC_VARARGS, CREATE_METHOD_NAME,
+					"([Ljava/lang/Object;)Ljava/lang/Object;", null, null);
+			mv.visitCode();
+			mv.visitTypeInsn(NEW, objectClassResourcePath);
+			mv.visitInsn(DUP);
+
+			Constructor<?> ctor = key.getConstructor();
+			Class<?>[] parameterTypes = ctor.getParameterTypes();
+			for (int i = 0; i < parameterTypes.length; i++) {
+				mv.visitVarInsn(ALOAD, 1);
+				mv.visitIntInsn(BIPUSH, i);
+				mv.visitInsn(AALOAD);
+				mv.visitTypeInsn(CHECKCAST, Type.getInternalName(parameterTypes[i]));
+			}
+			mv.visitMethodInsn(INVOKESPECIAL, objectClassResourcePath, INIT, Type.getConstructorDescriptor(ctor), false);
+
+			mv.visitInsn(ARETURN);
+			mv.visitMaxs(0, 0); // (0, 0) = computed via ClassWriter.COMPUTE_MAXS
+			mv.visitEnd();
 		}
 	}
 }
