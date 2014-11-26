23fe1df4b9544c6a7eb3a429ff921e60d5d29e91
Christoph Strobl <cstrobl@pivotal.io>
2014-11-26 08:37:00 +0100

    DATACMNS-525 - Add JavaDoc for KeyValueCallback.
    Original pull request: #95.

--- src/main/java/org/springframework/data/keyvalue/core/KeyValueCallback.java
+++ src/main/java/org/springframework/data/keyvalue/core/KeyValueCallback.java
@@ -16,11 +16,21 @@
 package org.springframework.data.keyvalue.core;
 
 /**
+ * Generic callback interface for code that operates on a {@link KeyValueAdapter}. This is particularly useful for
+ * delegating code that needs to work closely on the underlying key/value store implementation.
+ * 
  * @author Christoph Strobl
  * @since 1.10
  * @param <T>
  */
 public interface KeyValueCallback<T> {
 
+	/**
+	 * Gets called by {@code KeyValueTemplate#execute(KeyValueCallback)}. Allows for returning a result object created
+	 * within the callback, i.e. a domain object or a collection of domain objects.
+	 * 
+	 * @param adapter
+	 * @return
+	 */
 	T doInKeyValue(KeyValueAdapter adapter);
 }
