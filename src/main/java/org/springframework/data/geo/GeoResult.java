6f7d8489bf12fbe946f7286b5dcfbd98882fa560
Thomas Darimont <tdarimont@pivotal.io>
2015-03-23 11:07:31 +0100

    DATACMNS-654 - Polishing.
    Added missing documentation for geospatial return types.
    Fixed typo in GeoResult JavaDoc.

--- src/main/java/org/springframework/data/geo/GeoResult.java
+++ src/main/java/org/springframework/data/geo/GeoResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2014 the original author or authors.
+ * Copyright 2011-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.io.Serializable;
 import org.springframework.util.Assert;
 
 /**
- * Calue object capturing some arbitrary object plus a distance.
+ * Value object capturing some arbitrary object plus a distance.
  * 
  * @author Oliver Gierke
  * @author Thomas Darimont
