97b8d45eeeeea74a32c24adae7d9af531cec9e85
Oliver Gierke <ogierke@pivotal.io>
2015-04-13 13:04:59 +0200

    DATACMNS-681 - Removed some compiler warnings.
    Removed unused constants. Removed usage of deprecated method in RepositoryBeanDefinitionParser.

--- src/main/java/org/springframework/data/repository/query/Parameters.java
+++ src/main/java/org/springframework/data/repository/query/Parameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2014 the original author or authors.
+ * Copyright 2008-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,7 @@ import org.springframework.util.Assert;
  */
 public abstract class Parameters<S extends Parameters<S, T>, T extends Parameter> implements Iterable<T> {
 
+	@SuppressWarnings("unchecked")//
 	public static final List<Class<?>> TYPES = Arrays.asList(Pageable.class, Sort.class);
 
 	private static final String PARAM_ON_SPECIAL = format("You must not user @%s on a parameter typed %s or %s",
