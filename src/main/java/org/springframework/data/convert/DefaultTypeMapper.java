a36dbe90221924f3b883ac19778d19b2c3344655
Oliver Gierke <ogierke@pivotal.io>
2016-02-09 14:19:38 +0100

    DATACMNS-815 - Made static type cache non-static in SimpleTypeInformationMapper.
    We now use a non-static cache for TypeInformation instances in SimpleTypeInformationMapper to make sure Spring Boot's development tools can correctly reload classes. Deprecated the use of the static instance of SimpleTypeInformationMapper in favor instantiating it directly.

--- src/main/java/org/springframework/data/convert/DefaultTypeMapper.java
+++ src/main/java/org/springframework/data/convert/DefaultTypeMapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 the original author or authors.
+ * Copyright 2011-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ public class DefaultTypeMapper<S> implements TypeMapper<S> {
 	 * @param accessor must not be {@literal null}.
 	 */
 	public DefaultTypeMapper(TypeAliasAccessor<S> accessor) {
-		this(accessor, Arrays.asList(SimpleTypeInformationMapper.INSTANCE));
+		this(accessor, Arrays.asList(new SimpleTypeInformationMapper()));
 	}
 
 	/**
