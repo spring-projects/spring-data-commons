09ab40939146f40aeb79162b738fc2f309fa1563
Oliver Gierke <ogierke@pivotal.io>
2016-02-20 20:32:53 +0100

    DATACMNS-820 - Polishing.
    Fixed indentation to use tabs instead of spaces. Tweaked structure of if-clauses in PropertyAccessingMethodInterceptor to reduce nesting. Made helper method static.
    
    Restructured test cases slightly and used ExpectedException rule to verify exceptions. Moved newly introduced test methods more to the end of the file (new tests last).
    
    Added author and copyright year extensions where necessary.
    
    Original pull request: #155.

--- src/main/java/org/springframework/data/projection/SpelAwareProxyProjectionFactory.java
+++ src/main/java/org/springframework/data/projection/SpelAwareProxyProjectionFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ import org.springframework.util.ReflectionUtils;
  * 
  * @author Oliver Gierke
  * @author Thomas Darimont
+ * @author Mark Paluch
  * @since 1.10
  */
 public class SpelAwareProxyProjectionFactory extends ProxyProjectionFactory implements BeanFactoryAware {
