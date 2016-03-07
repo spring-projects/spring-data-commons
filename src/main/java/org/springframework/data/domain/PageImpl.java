fecfdd2eb9388bddccdfc3c4f349196d9552c3ea
Oliver Gierke <ogierke@pivotal.io>
2016-03-07 11:32:41 +0100

    DATACMNS-812 - Polishing.
    Formatting and extension of copyright years.
    
    Original pull request: #154.

--- src/main/java/org/springframework/data/domain/PageImpl.java
+++ src/main/java/org/springframework/data/domain/PageImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2014 the original author or authors.
+ * Copyright 2008-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,7 +118,7 @@ public class PageImpl<T> extends Chunk<T> implements Page<T> {
 			contentType = content.get(0).getClass().getName();
 		}
 
-		return String.format("Page %s of %d containing %s instances", getNumber()+1, getTotalPages(), contentType);
+		return String.format("Page %s of %d containing %s instances", getNumber() + 1, getTotalPages(), contentType);
 	}
 
 	/*
