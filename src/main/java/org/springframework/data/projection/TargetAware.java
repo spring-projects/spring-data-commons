c18194aac5ff3656da554323d4c363925ceda1ff
Oliver Gierke <ogierke@pivotal.io>
2016-09-09 19:41:56 +0200

    DATACMNS-909 - Mask getDecoratedClass() of Spring 4.3's DecoratingProxy.
    The proxies created for projections now defensively mask getDecoratedClass(), a method declared on DecoratingProxy, an interface that JDK proxies on Spring 4.3 will implement.

--- src/main/java/org/springframework/data/projection/TargetAware.java
+++ src/main/java/org/springframework/data/projection/TargetAware.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,4 +41,13 @@ public interface TargetAware extends org.springframework.aop.TargetClassAware, R
 	 */
 	@JsonIgnore
 	Object getTarget();
+
+	/**
+	 * Re-declaration of Spring Framework 4.3's {@link org.springframework.core.DecoratingProxy#getDecoratedClass()} so
+	 * that we can exclude it from Jackson serialization.
+	 * 
+	 * @return
+	 */
+	@JsonIgnore
+	Class<?> getDecoratedClass();
 }
