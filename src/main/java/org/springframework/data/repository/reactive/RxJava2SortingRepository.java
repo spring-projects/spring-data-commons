39ab1b35551ba440b0a7e8478449fc7bf72aab55
Mark Paluch <mpaluch@pivotal.io>
2017-02-08 10:36:40 +0100

    DATACMNS-988 - Provide RxJava 2 Repository interfaces.
    Provide RxJava 2 Repository interfaces using back-pressure enabled Flowable as default return type for multi-element queries and Maybe for queries that can complete without emitting a value.
    
    Original Pull Request: #194

---
+++ src/main/java/org/springframework/data/repository/reactive/RxJava2SortingRepository.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.repository.reactive;
+
+import io.reactivex.Flowable;
+
+import java.io.Serializable;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.NoRepositoryBean;
+
+/**
+ * Extension of {@link RxJava2CrudRepository} to provide additional methods to retrieve entities using the sorting
+ * abstraction.
+ *
+ * @author Mark Paluch
+ * @since 2.0
+ * @see Sort
+ * @see Flowable
+ * @see RxJava2CrudRepository
+ */
+@NoRepositoryBean
+public interface RxJava2SortingRepository<T, ID extends Serializable> extends RxJava2CrudRepository<T, ID> {
+
+	/**
+	 * Returns all entities sorted by the given options.
+	 *
+	 * @param sort must not be {@literal null}.
+	 * @return all entities sorted by the given options.
+	 */
+	Flowable<T> findAll(Sort sort);
+}
