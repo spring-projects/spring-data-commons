e9438ed36e8e41ddccf1ae3534ac5d9682a2c115
Oliver Gierke <ogierke@pivotal.io>
2017-04-20 10:09:10 +0200

    DATACMNS-1034 - Introduced API to easily register converters using Java 8 lambdas.
    Introduced ConverterBuilder which exposes API to register Spring GenericConverters for the use with a store module's CustomConversions. This allows simple registration of such converters using Java 8 lambdas.
    
    ConverterAware converters = ConverterBuilder.reading(String.class, Long.class,
      it -> Long.valueOf(it)).andWriting(it -> Object::toString);
    
    The setup can also be done from the reading side which would just need the method invocations inverted. The resulting ConverterAware will expose the registered converters so that they can be easily handed to a CustomConversions instance. Partial creation of either reading or writing converters is possible, too with the returned instance then only exposing one of the two converters.

---
+++ src/main/java/org/springframework/data/convert/DefaultConverterBuilder.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.convert;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Wither;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.GenericConverter;
+import org.springframework.core.convert.converter.GenericConverter.ConvertiblePair;
+import org.springframework.data.convert.ConverterBuilder.ConverterAware;
+import org.springframework.data.convert.ConverterBuilder.ReadingConverterBuilder;
+import org.springframework.data.convert.ConverterBuilder.WritingConverterBuilder;
+import org.springframework.data.util.Optionals;
+
+/**
+ * Builder to easily set up (bi-directional) {@link Converter} instances for Spring Data type mapping using Lambdas. Use
+ * factory methods on {@link ConverterBuilder} to create instances of this class.
+ * 
+ * @author Oliver Gierke
+ * @since 2.0
+ * @see ConverterBuilder#writing(Class, Class, Function)
+ * @see ConverterBuilder#reading(Class, Class, Function)
+ * @soundtrack John Mayer - Still Feel Like Your Man (The Search for Everything)
+ */
+@Wither(AccessLevel.PACKAGE)
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+class DefaultConverterBuilder<S, T>
+		implements ConverterAware, ReadingConverterBuilder<T, S>, WritingConverterBuilder<S, T> {
+
+	private final @NonNull ConvertiblePair convertiblePair;
+	private final @NonNull Optional<Function<? super S, ? extends T>> writing;
+	private final @NonNull Optional<Function<? super T, ? extends S>> reading;
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.convert.WritingConverterBuilder#andReading(java.util.function.Function)
+	 */
+	@Override
+	public ConverterAware andReading(Function<? super T, ? extends S> function) {
+		return withReading(Optional.of(function));
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.convert.ReadingConverterBuilder#andWriting(java.util.function.Function)
+	 */
+	@Override
+	public ConverterAware andWriting(Function<? super S, ? extends T> function) {
+		return withWriting(Optional.of(function));
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.convert.ReadingConverterBuilder#getRequiredReadingConverter()
+	 */
+	@Override
+	public GenericConverter getReadingConverter() {
+		return getOptionalReadingConverter()
+				.orElseThrow(() -> new IllegalStateException("No reading converter specified!"));
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.convert.WritingConverterBuilder#getRequiredWritingConverter()
+	 */
+	@Override
+	public GenericConverter getWritingConverter() {
+		return getOptionalWritingConverter()
+				.orElseThrow(() -> new IllegalStateException("No writing converter specified!"));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.convert.ConverterBuilder#getConverters()
+	 */
+	@Override
+	public Set<GenericConverter> getConverters() {
+		return Optionals.toStream(getOptionalReadingConverter(), getOptionalWritingConverter()).collect(Collectors.toSet());
+	}
+
+	private Optional<GenericConverter> getOptionalReadingConverter() {
+		return reading.map(it -> new ConfigurableGenericConverter.Reading<>(convertiblePair, it));
+	}
+
+	private Optional<GenericConverter> getOptionalWritingConverter() {
+		return writing.map(it -> new ConfigurableGenericConverter.Writing<>(invertedPair(), it));
+	}
+
+	private ConvertiblePair invertedPair() {
+		return new ConvertiblePair(convertiblePair.getTargetType(), convertiblePair.getSourceType());
+	}
+
+	@RequiredArgsConstructor
+	@EqualsAndHashCode
+	private static class ConfigurableGenericConverter<S, T> implements GenericConverter {
+
+		private final ConvertiblePair convertiblePair;
+		private final Function<? super S, ? extends T> function;
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.springframework.core.convert.converter.GenericConverter#convert(java.lang.Object, org.springframework.core.convert.TypeDescriptor, org.springframework.core.convert.TypeDescriptor)
+		 */
+		@Override
+		@SuppressWarnings("unchecked")
+		public Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
+			return function.apply((S) source);
+		}
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.springframework.core.convert.converter.GenericConverter#getConvertibleTypes()
+		 */
+		@Override
+		public Set<ConvertiblePair> getConvertibleTypes() {
+			return Collections.singleton(convertiblePair);
+		}
+
+		@WritingConverter
+		private static class Writing<S, T> extends ConfigurableGenericConverter<S, T> {
+
+			public Writing(ConvertiblePair convertiblePair, Function<? super S, ? extends T> function) {
+				super(convertiblePair, function);
+			}
+		}
+
+		@ReadingConverter
+		private static class Reading<S, T> extends ConfigurableGenericConverter<S, T> {
+
+			public Reading(ConvertiblePair convertiblePair, Function<? super S, ? extends T> function) {
+				super(convertiblePair, function);
+			}
+		}
+	}
+}
---
+++ src/test/java/org/springframework/data/convert/ConverterBuilderUnitTests.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.convert;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.mockito.internal.util.Supplier;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.GenericConverter;
+import org.springframework.core.convert.converter.GenericConverter.ConvertiblePair;
+import org.springframework.data.convert.ConverterBuilder.ConverterAware;
+import org.springframework.data.convert.ConverterBuilder.ReadingConverterBuilder;
+import org.springframework.data.convert.ConverterBuilder.WritingConverterBuilder;
+
+/**
+ * Unit tests for {@link DefaultConverterBuilder}.
+ * 
+ * @author Oliver Gierke
+ * @since 2.0
+ * @soundtrack John Mayer - In the Blood (The Search for Everything)
+ */
+public class ConverterBuilderUnitTests {
+
+	@Test // DATACMNS-1034
+	public void setsUpBidirectionalConvertersFromReading() {
+
+		ConverterAware builder = ConverterBuilder.reading(String.class, Long.class, it -> Long.valueOf(it))
+				.andWriting(Object::toString);
+
+		assertConverter(builder.getReadingConverter(), "1", 1L);
+		assertConverter(builder.getWritingConverter(), 1L, "1");
+	}
+
+	@Test // DATACMNS-1034
+	public void setsUpBidirectionalConvertersFromWriting() {
+
+		ConverterAware builder = ConverterBuilder.writing(Long.class, String.class, Object::toString)
+				.andReading(it -> Long.valueOf(it));
+
+		assertConverter(builder.getReadingConverter(), "1", 1L);
+		assertConverter(builder.getWritingConverter(), 1L, "1");
+	}
+
+	@Test // DATACMNS-1034
+	public void setsUpReadingConverter() {
+
+		ReadingConverterBuilder<String, Long> builder = ConverterBuilder.reading(String.class, Long.class,
+				string -> Long.valueOf(string));
+
+		assertConverter(builder.getReadingConverter(), "1", 1L);
+		assertOnlyConverter(builder, builder::getReadingConverter);
+	}
+
+	@Test // DATACMNS-1034
+	public void setsUpWritingConverter() {
+
+		WritingConverterBuilder<Long, String> builder = ConverterBuilder.writing(Long.class, String.class,
+				Object::toString);
+
+		assertConverter(builder.getWritingConverter(), 1L, "1");
+		assertOnlyConverter(builder, builder::getWritingConverter);
+	}
+
+	private static void assertConverter(GenericConverter converter, Object source, Object target) {
+
+		assertThat(converter.getConvertibleTypes())
+				.containsExactly(new ConvertiblePair(source.getClass(), target.getClass()));
+		assertThat(converter.convert(source, TypeDescriptor.forObject(source), TypeDescriptor.forObject(target)))
+				.isEqualTo(target);
+	}
+
+	private static void assertOnlyConverter(ConverterBuilder builder, Supplier<GenericConverter> supplier) {
+		assertThat(builder.getConverters()).containsExactly(supplier.get());
+	}
+}
