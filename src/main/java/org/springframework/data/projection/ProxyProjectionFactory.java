d7011a3da5de3110846ea799c90cfce708780e40
Oliver Gierke <ogierke@pivotal.io>
2017-07-20 14:29:54 +0200

    DATACMNS-1121 - ProxyProjectionFactory now returns instance as is if it implements the target.
    If you previously asked ProxyProjectionFactory for a proxy of an interface which the target instance already implements, it created a proxy although it could've returned the instance as is. It's now actually doing that avoiding superfluous proxy creation.

--- src/main/java/org/springframework/data/projection/ProxyProjectionFactory.java
+++ src/main/java/org/springframework/data/projection/ProxyProjectionFactory.java
@@ -109,8 +109,8 @@ class ProxyProjectionFactory implements ProjectionFactory, ResourceLoaderAware, 
 		Assert.notNull(projectionType, "Projection type must not be null!");
 		Assert.isTrue(projectionType.isInterface(), "Projection type must be an interface!");
 
-		if (source == null) {
-			return null;
+		if (source == null || projectionType.isInstance(source)) {
+			return (T) source;
 		}
 
 		ProxyFactory factory = new ProxyFactory();
