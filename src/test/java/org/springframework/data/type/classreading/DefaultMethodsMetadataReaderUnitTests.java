5eb10a081765337974bf2906e4477483d2906c66
Oliver Gierke <ogierke@pivotal.io>
2018-01-12 10:31:44 +0100

    DATACMNS-1206 - Polishing.
    Moved PropertyDescriptor lookup into dedicated subclass to group functionality around the type and MethodsMetadata instances. Extracted individual stream handling steps into dedicated methods for better understandability.
    
    Moved MethodsMetadataReader into classreading package for symmetry with Spring Framework's metadata arrangement. Removed manually declared getters in DefaultMethodsMetadataReader in favor of Lombok getters. Inlined MethodsMetadataReadingVisitor into DefaultMethodsMetadataReader as it's only used there.
    
    Original pull request: #263.

--- src/test/java/org/springframework/data/type/classreading/DefaultMethodsMetadataReaderUnitTests.java
+++ src/test/java/org/springframework/data/type/classreading/DefaultMethodsMetadataReaderUnitTests.java
@@ -23,7 +23,6 @@ import java.util.Iterator;
 import org.junit.Test;
 import org.springframework.core.type.MethodMetadata;
 import org.springframework.data.type.MethodsMetadata;
-import org.springframework.data.type.MethodsMetadataReader;
 
 /**
  * Unit tests for {@link DefaultMethodsMetadataReader}.
