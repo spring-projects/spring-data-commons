0d39693c94c6bc8e7371d6e53e47e6f4dadceb9b
Mark Paluch <mpaluch@pivotal.io>
2018-08-17 10:51:39 +0200

    DATACMNS-1376 - Fix illegal access warning in DefaultMethodInvokingMethodInterceptor on Java 9 and higher.
    We now attempt to use private MethodHandles lookup as the first mechanism to resolve a MethodHandle for default interface methods and fall back to reflection-based Lookup construction if private lookup is not available. Reflective availability is checked lazily to prevent illegal access on enum constant construction. This approach prevents an illegal access which was logged by attempting a reflection-based lookup first.
    
    We also introduced a FALLBACK mechanism to split encapsulated access from a fallback mechanism.
    
    Original pull request: #307.

---
+++ src/test/java/org/springframework/data/projection/DefaultMethodInvokingMethodInterceptorUnitTests.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.projection;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assume.*;
+
+import org.junit.Test;
+import org.springframework.data.projection.DefaultMethodInvokingMethodInterceptor.MethodHandleLookup;
+import org.springframework.data.util.Version;
+
+/**
+ * Unit tests for {@link DefaultMethodInvokingMethodInterceptor}.
+ *
+ * @author Mark Paluch
+ */
+public class DefaultMethodInvokingMethodInterceptorUnitTests {
+
+	@Test // DATACMNS-1376
+	public void shouldApplyEncapsulatedLookupOnJava9AndHigher() {
+
+		Version version = Version.parse(System.getProperty("java.version"));
+
+		assumeTrue(version.isGreaterThanOrEqualTo(Version.parse("9.0")));
+
+		assertThat(MethodHandleLookup.getMethodHandleLookup()).isEqualTo(MethodHandleLookup.ENCAPSULATED);
+		assertThat(MethodHandleLookup.ENCAPSULATED.isAvailable()).isTrue();
+	}
+
+	@Test // DATACMNS-1376
+	public void shouldApplyOpenLookupOnJava8() {
+
+		Version version = Version.parse(System.getProperty("java.version"));
+
+		assumeTrue(version.isLessThan(Version.parse("1.8.9999")));
+
+		assertThat(MethodHandleLookup.getMethodHandleLookup()).isEqualTo(MethodHandleLookup.OPEN);
+		assertThat(MethodHandleLookup.OPEN.isAvailable()).isTrue();
+		assertThat(MethodHandleLookup.ENCAPSULATED.isAvailable()).isFalse();
+	}
+}
