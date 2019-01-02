e73f48ba4f8d6c0e111673300fcdf6679def724a
Mark Paluch <mpaluch@pivotal.io>
2019-01-02 12:09:25 +0100

    DATACMNS-1459 - Update copyright years to 2019.

--- src/test/java/org/springframework/data/repository/core/support/ReflectionEntityInformationUnitTests.java
+++ src/test/java/org/springframework/data/repository/core/support/ReflectionEntityInformationUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may
