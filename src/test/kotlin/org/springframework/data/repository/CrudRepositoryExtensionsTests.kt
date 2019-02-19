ccb9e987e8e5fb85b5d79be63021b543642905da
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-02-19 11:23:01 +0100

    DATACMNS-1483 - Migrate Kotlin tests to Mockk.
    Original pull request: #338.

--- src/test/kotlin/org/springframework/data/repository/CrudRepositoryExtensionsTests.kt
+++ src/test/kotlin/org/springframework/data/repository/CrudRepositoryExtensionsTests.kt
@@ -15,28 +15,30 @@
  */
 package org.springframework.data.repository
 
-import com.nhaarman.mockito_kotlin.verify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Answers
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 import org.springframework.data.repository.sample.User
+import java.util.*
+import kotlin.test.assertNull
 
 /**
  * Unit tests for CrudRepositoryExtensions.
  *
  * @author Sebastien Deleuze
  */
-@RunWith(MockitoJUnitRunner::class)
 class CrudRepositoryExtensionsTests {
 
-	@Mock(answer = Answers.RETURNS_MOCKS)
-	lateinit var repository: CrudRepository<User, String>
+	var repository = mockk<CrudRepository<User, String>>()
 
 	@Test // DATACMNS-1346
 	fun `CrudRepository#findByIdOrNull() extension should call its Java counterpart`() {
-		repository.findByIdOrNull("foo")
-		verify(repository).findById("foo")
+		val user = User()
+		every { repository.findById("foo") }.returnsMany(Optional.of(user), Optional.empty())
+		assertEquals(user, repository.findByIdOrNull("foo"))
+		assertNull(repository.findByIdOrNull("foo"))
+		verify(exactly = 2) { repository.findById("foo") }
 	}
 }
