63f8100a39ffc479a557aaa034b8eac81daa8be4
Greg Turnquist <gturnquist@pivotal.io>
2019-02-27 17:12:05 -0600

    DATACMNS-1462 - Polishing.

--- ci/build.sh
+++ ci/build.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-spring_commons_artifactory=$(pwd)/spring-commons-artifactory
+spring_data_commons_artifactory=$(pwd)/spring-data-commons-artifactory
 
-rm -rf $HOME/.m2/repository/org/springframework/commons 2> /dev/null || :
+rm -rf $HOME/.m2/repository/org/springframework/data/commons 2> /dev/null || :
 
-cd spring-commons-github
+cd spring-data-commons-github
 
 ./mvnw -Pdistribute -Dmaven.test.skip=true clean deploy \
-	-DaltDeploymentRepository=distribution::default::file://${spring_commons_artifactory}
+	-DaltDeploymentRepository=distribution::default::file://${spring_data_commons_artifactory}
--- ci/test.sh
+++ ci/test.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-rm -rf $HOME/.m2/repository/org/springframework/commons 2> /dev/null || :
+rm -rf $HOME/.m2/repository/org/springframework/data/commons 2> /dev/null || :
 
-cd spring-commons-github
+cd spring-data-commons-github
 
 ./mvnw clean dependency:list test -P${PROFILE} -Dsort
