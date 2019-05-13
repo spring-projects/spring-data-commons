97e41dd15399a733f1b265c0c75fc50f5b0eeecd
Greg Turnquist <gturnquist@pivotal.io>
2019-05-13 13:40:57 -0500

    DATACMNS-1536 - Introduce Jenkins.

--- ci/build.sh
+++ ci/build.sh
@@ -2,13 +2,4 @@
 
 set -euo pipefail
 
-[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
-
-spring_data_commons_artifactory=$(pwd)/spring-data-commons-artifactory
-
-rm -rf $HOME/.m2/repository/org/springframework/data/commons 2> /dev/null || :
-
-cd spring-data-commons-github
-
-./mvnw -Dmaven.test.skip=true clean deploy \
-	-DaltDeploymentRepository=distribution::default::file://${spring_data_commons_artifactory}
+./mvnw -P${PROFILE} -Dmaven.test.skip=true clean deploy -B
--- ci/test.sh
+++ ci/test.sh
@@ -2,10 +2,4 @@
 
 set -euo pipefail
 
-[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
-
-rm -rf $HOME/.m2/repository/org/springframework/data/commons 2> /dev/null || :
-
-cd spring-data-commons-github
-
-./mvnw clean dependency:list test -P${PROFILE} -Dsort
+./mvnw -P${PROFILE} clean dependency:list test -Dsort -B
