6d8040d8dabb95fb1bf1010b26101d298a14153f
Mark Paluch <mpaluch@pivotal.io>
2019-10-30 09:55:18 +0100

    DATACMNS-1601 - Polishing.
    Document throws on save(…). Align documentation for reactive repositories.
    
    Original pull request: #412.

--- src/main/java/org/springframework/data/repository/reactive/ReactiveCrudRepository.java
+++ src/main/java/org/springframework/data/repository/reactive/ReactiveCrudRepository.java
@@ -41,7 +41,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param entity must not be {@literal null}.
 	 * @return {@link Mono} emitting the saved entity.
-	 * @throws IllegalArgumentException in case the given {@code entity} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@literal entity} is {@literal null}.
 	 */
 	<S extends T> Mono<S> save(S entity);
 
@@ -50,7 +50,8 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param entities must not be {@literal null}.
 	 * @return {@link Flux} emitting the saved entities.
-	 * @throws IllegalArgumentException in case the given {@link Iterable} {@code entities} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
+	 *           {@literal null}.
 	 */
 	<S extends T> Flux<S> saveAll(Iterable<S> entities);
 
@@ -59,7 +60,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param entityStream must not be {@literal null}.
 	 * @return {@link Flux} emitting the saved entities.
-	 * @throws IllegalArgumentException in case the given {@code Publisher} {@code entityStream} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Publisher entityStream} is {@literal null}.
 	 */
 	<S extends T> Flux<S> saveAll(Publisher<S> entityStream);
 
@@ -68,7 +69,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param id must not be {@literal null}.
 	 * @return {@link Mono} emitting the entity with the given id or {@link Mono#empty()} if none found.
-	 * @throws IllegalArgumentException in case the given {@code id} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
 	 */
 	Mono<T> findById(ID id);
 
@@ -77,16 +78,16 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param id must not be {@literal null}. Uses the first emitted element to perform the find-query.
 	 * @return {@link Mono} emitting the entity with the given id or {@link Mono#empty()} if none found.
-	 * @throws IllegalArgumentException in case the given {@link Publisher} {@code id} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
 	 */
 	Mono<T> findById(Publisher<ID> id);
 
 	/**
-	 * Returns whether an entity with the id exists.
+	 * Returns whether an entity with the given {@literal id} exists.
 	 *
 	 * @param id must not be {@literal null}.
 	 * @return {@link Mono} emitting {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-	 * @throws IllegalArgumentException in case the given {@code id} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
 	 */
 	Mono<Boolean> existsById(ID id);
 
@@ -95,8 +96,8 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 * element to perform the exists-query.
 	 *
 	 * @param id must not be {@literal null}.
-	 * @return {@link Mono} emitting {@literal true} if an entity with the given id exists, {@literal false} otherwise
-	 * @throws IllegalArgumentException in case the given {@link Publisher} {@code id} is {@literal null}.
+	 * @return {@link Mono} emitting {@literal true} if an entity with the given id exists, {@literal false} otherwise.
+	 * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
 	 */
 	Mono<Boolean> existsById(Publisher<ID> id);
 
@@ -108,20 +109,29 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	Flux<T> findAll();
 
 	/**
-	 * Returns all instances with the given IDs.
+	 * Returns all instances of the type {@code T} with the given IDs.
+	 * <p>
+	 * If some or all ids are not found, no entities are returned for these IDs.
+	 * <p>
+	 * Note that the order of elements in the result is not guaranteed.
 	 *
-	 * @param ids must not be {@literal null}.
-	 * @return {@link Flux} emitting the found entities.
-	 * @throws IllegalArgumentException in case the given {@link Iterable} {@code ids} is {@literal null}.
+	 * @param ids must not be {@literal null} nor contain any {@literal null} values.
+	 * @return {@link Flux} emitting the found entities. The size can be equal or less than the number of given
+	 *         {@literal ids}.
+	 * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
 	 */
 	Flux<T> findAllById(Iterable<ID> ids);
 
 	/**
-	 * Returns all instances of the type with the given IDs supplied by a {@link Publisher}.
+	 * Returns all instances of the type {@code T} with the given IDs supplied by a {@link Publisher}.
+	 * <p>
+	 * If some or all ids are not found, no entities are returned for these IDs.
+	 * <p>
+	 * Note that the order of elements in the result is not guaranteed.
 	 *
 	 * @param idStream must not be {@literal null}.
 	 * @return {@link Flux} emitting the found entities.
-	 * @throws IllegalArgumentException in case the given {@link Publisher} {@code idStream} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Publisher idStream} is {@literal null}.
 	 */
 	Flux<T> findAllById(Publisher<ID> idStream);
 
@@ -137,7 +147,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param id must not be {@literal null}.
 	 * @return {@link Mono} signaling when operation has completed.
-	 * @throws IllegalArgumentException in case the given {@code id} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
 	 */
 	Mono<Void> deleteById(ID id);
 
@@ -146,7 +156,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param id must not be {@literal null}.
 	 * @return {@link Mono} signaling when operation has completed.
-	 * @throws IllegalArgumentException in case the given {@link Publisher} {@code id} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
 	 */
 	Mono<Void> deleteById(Publisher<ID> id);
 
@@ -164,7 +174,8 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param entities must not be {@literal null}.
 	 * @return {@link Mono} signaling when operation has completed.
-	 * @throws IllegalArgumentException in case the given {@link Iterable} {@code entities} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
+	 *           {@literal null}.
 	 */
 	Mono<Void> deleteAll(Iterable<? extends T> entities);
 
@@ -173,7 +184,7 @@ public interface ReactiveCrudRepository<T, ID> extends Repository<T, ID> {
 	 *
 	 * @param entityStream must not be {@literal null}.
 	 * @return {@link Mono} signaling when operation has completed.
-	 * @throws IllegalArgumentException in case the given {@link Publisher} {@code entityStream} is {@literal null}.
+	 * @throws IllegalArgumentException in case the given {@link Publisher entityStream} is {@literal null}.
 	 */
 	Mono<Void> deleteAll(Publisher<? extends T> entityStream);
 
