cd5ad401dac4ba84c321592477be43f74f32011a
Phillip Webb <pwebb@vmware.com>
2020-06-09 12:33:31 -0700

    Fix malformed copyright headers
    Fix a few copyright headers that had formatting or date issues.

--- src/test/java/org/springframework/data/repository/support/RepositoriesUnitTests.java
+++ src/test/java/org/springframework/data/repository/support/RepositoriesUnitTests.java
@@ -2,9 +2,7 @@
  * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except
-import org.springframework.data.repository.core.support.DefaultRepositoryMetadata;
- in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
