6afe32235b4b76697fce2863058d306360b83150
Mark Paluch <mpaluch@pivotal.io>
2020-07-21 14:51:23 +0200

    DATACMNS-1772 - Remove SpringDataAnnotationBeanNameGenerator and use BeanNameGenerator directly.
    We now invoke BeanNameGenerator directly without additional indirections. Previously, SpringDataAnnotationBeanNameGenerator was calling BeanNameGenerator using null for BeanDefinitionRegistry which caused downstream null dereference.

--- src/main/java/org/springframework/data/repository/config/RepositoryConfigurationSourceSupport.java
+++ src/main/java/org/springframework/data/repository/config/RepositoryConfigurationSourceSupport.java
@@ -61,7 +61,7 @@ public abstract class RepositoryConfigurationSourceSupport implements Repository
 		Assert.notNull(registry, "BeanDefinitionRegistry must not be null!");
 
 		this.environment = environment;
-		this.beanNameGenerator = new RepositoryBeanNameGenerator(classLoader, generator);
+		this.beanNameGenerator = new RepositoryBeanNameGenerator(classLoader, generator, registry);
 		this.registry = registry;
 	}
 
