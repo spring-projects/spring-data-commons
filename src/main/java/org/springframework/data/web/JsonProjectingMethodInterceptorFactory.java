efe93e31b140b0603132a88288ca05e751efdb9a
Mark Paluch <mpaluch@vmware.com>
2021-01-12 15:47:00 +0100

    Polishing.
    Reuse definitive path flag without recompile the expression.
    
    See #2270

--- src/main/java/org/springframework/data/web/JsonProjectingMethodInterceptorFactory.java
+++ src/main/java/org/springframework/data/web/JsonProjectingMethodInterceptorFactory.java
@@ -158,13 +158,14 @@ public class JsonProjectingMethodInterceptorFactory implements MethodInterceptor
 						return isCollectionResult && !(nested instanceof Collection) ? result : nested;
 					}
 
-					type = isCollectionResult && JsonPath.isPathDefinite(jsonPath)
+					boolean definitePath = JsonPath.isPathDefinite(jsonPath);
+					type = isCollectionResult && definitePath
 							? ResolvableType.forClassWithGenerics(List.class, type)
 							: type;
 
 					List<?> result = (List<?>) context.read(jsonPath, new ResolvableTypeRef(type));
 
-					if (isCollectionResult && JsonPath.isPathDefinite(jsonPath)) {
+					if (isCollectionResult && definitePath) {
 						result = (List<?>) result.get(0);
 					}
 
