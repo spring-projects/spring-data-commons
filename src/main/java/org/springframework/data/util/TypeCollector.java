a382350100060b323e84f0badbdfe46c2d623a28
Christoph Strobl <cstrobl@vmware.com>
2022-12-06 14:41:59 +0100

    Exclude local & anonymous classes from type inspection
    Also update the type filter that would not mach types directly present in the given package.
    Modify type contribution method to allow store specific override.

--- src/main/java/org/springframework/data/util/TypeCollector.java
+++ src/main/java/org/springframework/data/util/TypeCollector.java
@@ -48,16 +48,18 @@ public class TypeCollector {
 
 	private static final Log logger = LogFactory.getLog(TypeCollector.class);
 
-	static final Set<String> EXCLUDED_DOMAINS = new HashSet<>(Arrays.asList("java", "sun.", "jdk.", "reactor.",
-			"kotlinx.", "kotlin.", "org.springframework.core.", "org.springframework.data.mapping.",
-			"org.springframework.data.repository.", "org.springframework.boot.", "org.springframework.core."));
+	static final Set<String> EXCLUDED_DOMAINS = new HashSet<>(
+			Arrays.asList("java", "sun.", "jdk.", "reactor.", "kotlinx.", "kotlin.", "org.springframework.core.",
+					"org.springframework.data.mapping.", "org.springframework.data.repository.", "org.springframework.boot.",
+					"org.springframework.context.", "org.springframework.beans."));
 
 	private final Predicate<Class<?>> excludedDomainsFilter = type -> {
-		String packageName = type.getPackageName();
+		String packageName = type.getPackageName() + ".";
 		return EXCLUDED_DOMAINS.stream().noneMatch(packageName::startsWith);
 	};
 
-	private Predicate<Class<?>> typeFilter = excludedDomainsFilter;
+	private Predicate<Class<?>> typeFilter = excludedDomainsFilter
+			.and(it -> !it.isLocalClass() && !it.isAnonymousClass());
 
 	private final Predicate<Method> methodFilter = createMethodFilter();
 
