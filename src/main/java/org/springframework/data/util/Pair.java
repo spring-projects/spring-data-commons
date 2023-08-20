2d0730f99cf0ac5e645e622533740b5210aff61c
imhosseinzadeh <imhosseinzadeh@gmail.com>
2023-08-20 21:27:59 +0330

    Change Pair to a record.
    getFirst() and getSecond() marked for removal.

--- src/main/java/org/springframework/data/util/Pair.java
+++ src/main/java/org/springframework/data/util/Pair.java
@@ -15,104 +15,71 @@
  */
 package org.springframework.data.util;
 
+import org.springframework.util.Assert;
+
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
-import org.springframework.util.ObjectUtils;
-
 /**
  * A tuple of things.
  *
+ * @param <S> Type of the first thing.
+ * @param <T> Type of the second thing.
  * @author Tobias Trelle
  * @author Oliver Gierke
  * @author Christoph Strobl
- * @param <S> Type of the first thing.
- * @param <T> Type of the second thing.
+ * @author Iman Hosseinzadeh
  * @since 1.12
  */
-public final class Pair<S, T> {
+public record Pair<S, T>(S first, T second) {
+    public Pair {
 
-	private final S first;
-	private final T second;
+        Assert.notNull(first, "First must not be null");
+        Assert.notNull(second, "Second must not be null");
+    }
 
-	private Pair(S first, T second) {
+    /**
+     * Creates a new {@link Pair} for the given elements.
+     *
+     * @param first  must not be {@literal null}.
+     * @param second must not be {@literal null}.
+     * @return
+     */
+    public static <S, T> Pair<S, T> of(S first, T second) {
+        return new Pair<>(first, second);
+    }
 
-		Assert.notNull(first, "First must not be null");
-		Assert.notNull(second, "Second must not be null");
+    /**
+     * A collector to create a {@link Map} from a {@link Stream} of {@link Pair}s.
+     *
+     * @return
+     */
+    public static <S, T> Collector<Pair<S, T>, ?, Map<S, T>> toMap() {
+        return Collectors.toMap(Pair::first, Pair::second);
+    }
 
-		this.first = first;
-		this.second = second;
-	}
+    /**
+     * @return
+     * @deprecated for removal. Use {@link #first()} instead.
+     */
+    @Deprecated(since = "3.2", forRemoval = true)
+    public S getFirst() {
+        return this.first;
+    }
 
-	/**
-	 * Creates a new {@link Pair} for the given elements.
-	 *
-	 * @param first must not be {@literal null}.
-	 * @param second must not be {@literal null}.
-	 * @return
-	 */
-	public static <S, T> Pair<S, T> of(S first, T second) {
-		return new Pair<>(first, second);
-	}
+    /**
+     * @return
+     * @deprecated for removal. Use {@link #second()} instead.
+     */
+    @Deprecated(since = "3.2", forRemoval = true)
+    public T getSecond() {
+        return this.second;
+    }
 
-	/**
-	 * Returns the first element of the {@link Pair}.
-	 *
-	 * @return
-	 */
-	public S getFirst() {
-		return first;
-	}
-
-	/**
-	 * Returns the second element of the {@link Pair}.
-	 *
-	 * @return
-	 */
-	public T getSecond() {
-		return second;
-	}
-
-	/**
-	 * A collector to create a {@link Map} from a {@link Stream} of {@link Pair}s.
-	 *
-	 * @return
-	 */
-	public static <S, T> Collector<Pair<S, T>, ?, Map<S, T>> toMap() {
-		return Collectors.toMap(Pair::getFirst, Pair::getSecond);
-	}
-
-	@Override
-	public boolean equals(@Nullable Object o) {
-
-		if (this == o) {
-			return true;
-		}
-
-		if (!(o instanceof Pair<?, ?> pair)) {
-			return false;
-		}
-
-		if (!ObjectUtils.nullSafeEquals(first, pair.first)) {
-			return false;
-		}
-
-		return ObjectUtils.nullSafeEquals(second, pair.second);
-	}
-
-	@Override
-	public int hashCode() {
-		int result = ObjectUtils.nullSafeHashCode(first);
-		result = 31 * result + ObjectUtils.nullSafeHashCode(second);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("%s->%s", this.first, this.second);
-	}
+    @Override
+    public String toString() {
+        return String.format("%s->%s", this.first, this.second);
+    }
 }
