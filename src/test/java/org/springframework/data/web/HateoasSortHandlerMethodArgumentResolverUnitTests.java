3e0ab7aa1f4600d18161d764c194e7453ef4d3bd
Oliver Drotbohm <oliver.drotbohm@broadcom.com>
2023-12-12 21:12:21 +0100

    Revert "Properly render sort variable variable as composite."
    This reverts commit 40edfdfdd72ee662e64247da1d9af0495efca4e8 as the change has too many side effects to be included in a bugfix version.
    
    Related ticket: GH-2531.

--- src/test/java/org/springframework/data/web/HateoasSortHandlerMethodArgumentResolverUnitTests.java
+++ src/test/java/org/springframework/data/web/HateoasSortHandlerMethodArgumentResolverUnitTests.java
@@ -29,7 +29,6 @@ import org.springframework.web.util.UriComponentsBuilder;
  * Unit tests for {@link HateoasSortHandlerMethodArgumentResolver}
  *
  * @author Oliver Gierke
- * @author Julien Béti
  */
 class HateoasSortHandlerMethodArgumentResolverUnitTests extends SortHandlerMethodArgumentResolverUnitTests {
 
@@ -48,13 +47,13 @@ class HateoasSortHandlerMethodArgumentResolverUnitTests extends SortHandlerMetho
 		assertUriStringFor(SORT, "/?sort=firstname,lastname,desc", "/?sort=foo,asc");
 	}
 
-	@Test // DATACMNS-418, GH-2531
+	@Test // DATACMNS-418
 	void returnCorrectTemplateVariables() {
 
 		var uriComponents = UriComponentsBuilder.fromPath("/").build();
 
 		var resolver = new HateoasSortHandlerMethodArgumentResolver();
-		assertThat(resolver.getSortTemplateVariables(null, uriComponents).toString()).isEqualTo("{?sort*}");
+		assertThat(resolver.getSortTemplateVariables(null, uriComponents).toString()).isEqualTo("{?sort}");
 	}
 
 	private void assertUriStringFor(Sort sort, String expected) throws Exception {
