e5065ae191d2d09c1a3a1fdd5d0433914a06f825
Christoph Strobl <christoph.strobl@broadcom.com>
2024-01-15 14:34:05 +0100

    Add missing runtime hit for Unpaged required by PageModule.
    The PageModule is loading the Unpaged type via its name which requires additional reflection configuration for native images.

---
+++ src/main/java/org/springframework/data/web/aot/hint/WebRuntimeHints.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.web.aot.hint;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.data.web.config.SpringDataJacksonConfiguration.PageModule;
+import org.springframework.lang.Nullable;
+
+/**
+ * @author Christoph Strobl
+ * @since 3.2.3
+ */
+public class WebRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+
+		hints.reflection().registerType(TypeReference.of("org.springframework.data.domain.Unpaged"), hint -> {
+			hint.onReachableType(PageModule.class);
+		});
+	}
+}
