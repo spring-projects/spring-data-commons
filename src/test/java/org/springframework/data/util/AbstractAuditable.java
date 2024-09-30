8ef195feed27a1de90ce69868702335344b8b022
Mark Paluch <mark.paluch@broadcom.com>
2024-09-30 09:23:19 +0200

    Introduce `BasicPropertyDescriptor` to `KotlinBeanInfoFactory` to bypass early property type validation.
    Closes #3167

---
+++ src/test/java/org/springframework/data/util/AbstractAuditable.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.lang.Nullable;
+
+public abstract class AbstractAuditable {
+
+	private @Nullable Date createdDate;
+
+	public Optional<LocalDateTime> getCreatedDate() {
+		return null == createdDate ? Optional.empty() : Optional.of(LocalDateTime.now());
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = Date.from(createdDate.atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+}
