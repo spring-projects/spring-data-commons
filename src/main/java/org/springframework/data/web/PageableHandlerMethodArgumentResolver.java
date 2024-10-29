579701a793d22809923043b458a8dd11d70afe72
Mark Paluch <mark.paluch@broadcom.com>
2024-10-29 09:25:16 +0100

    Polishing.
    Refine Javadoc and update ReactivePageableHandlerMethodArgumentResolver. Fix nullability annotations.
    
    See #3188

--- src/main/java/org/springframework/data/web/PageableHandlerMethodArgumentResolver.java
+++ src/main/java/org/springframework/data/web/PageableHandlerMethodArgumentResolver.java
@@ -26,7 +26,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
  * Extracts paging information from web requests and thus allows injecting {@link Pageable} instances into controller
- * methods. Request properties to be parsed can be configured.
+ * methods. Request properties to be parsed can be configured defaulting to {@code page} for the page number and
+ * {@code size} for the page size.
+ * <p>
+ * Parameters can be {@link #setPrefix(String) prefixed} to disambiguate from other parameters in the request if
+ * necessary.
  *
  * @author Oliver Gierke
  * @author Nick Williams
@@ -39,7 +43,8 @@ public class PageableHandlerMethodArgumentResolver extends PageableHandlerMethod
 		implements PageableArgumentResolver {
 
 	private static final SortHandlerMethodArgumentResolver DEFAULT_SORT_RESOLVER = new SortHandlerMethodArgumentResolver();
-	private SortArgumentResolver sortResolver;
+
+	private final SortArgumentResolver sortResolver;
 
 	/**
 	 * Constructs an instance of this resolved with a default {@link SortHandlerMethodArgumentResolver}.
