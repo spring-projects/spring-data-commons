587cd70646ec8fccac68d8e4645c7a993bdfdd96
Mark Paluch <mark.paluch@broadcom.com>
2024-11-19 11:39:17 +0100

    Polishing.
    Mark PersistenceConstructor for removal.
    
    See #3207

--- src/main/java/org/springframework/data/annotation/PersistenceConstructor.java
+++ src/main/java/org/springframework/data/annotation/PersistenceConstructor.java
@@ -31,5 +31,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE })
 @PersistenceCreator
-@Deprecated
+@Deprecated(forRemoval = true)
 public @interface PersistenceConstructor {}
