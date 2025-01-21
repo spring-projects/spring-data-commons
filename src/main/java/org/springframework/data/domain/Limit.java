c2f2cb80290174567a578ad370a46e15b3ccbbf7
Mark Paluch <mark.paluch@broadcom.com>
2025-01-21 10:24:57 +0100

    Document restrictions for zero Limit PageRequest translation.
    Closes #3102

--- src/main/java/org/springframework/data/domain/Limit.java
+++ src/main/java/org/springframework/data/domain/Limit.java
@@ -28,7 +28,11 @@ import org.springframework.lang.Nullable;
  * over using {@literal null} or {@link java.util.Optional#empty()} to indicate the absence of an actual {@link Limit}.
  * </p>
  * {@link Limit} itself does not make assumptions about the actual {@link #max()} value sign. This means that a negative
- * value may be valid within a defined context.
+ * value may be valid within a defined context. A zero limit can be useful in cases where the result is not needed but
+ * the underlying activity to compute results might be required.
+ * <p>
+ * Note that using a zero Limit with repository query methods returning {@link Page} is rejected because of a zero-page
+ * size.
  *
  * @author Christoph Strobl
  * @author Oliver Drotbohm
@@ -104,8 +108,7 @@ public sealed interface Limit permits Limited, Unlimited {
 				return false;
 			}
 
-			return this.isUnlimited() && that.isUnlimited()
-					|| max() == that.max();
+			return this.isUnlimited() && that.isUnlimited() || max() == that.max();
 		}
 
 		@Override
