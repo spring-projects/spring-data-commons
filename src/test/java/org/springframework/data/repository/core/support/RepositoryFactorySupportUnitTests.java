7c80c5da1827a9186c56b0fa654d465b0b1eb6a9
Mark Paluch <mark.paluch@broadcom.com>
2025-01-23 09:26:35 +0100

    Use Spring's `Nullness` utility to determine JSpecify nullness.
    We now use Nullness.forMethodParameter(…) to introspect method return types and argument types for nullness in addition to Spring's NonNullApi and JSR-305 annotations.
    
    Closes #3100

--- src/test/java/org/springframework/data/repository/core/support/RepositoryFactorySupportUnitTests.java
+++ src/test/java/org/springframework/data/repository/core/support/RepositoryFactorySupportUnitTests.java
@@ -32,6 +32,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.aopalliance.intercept.MethodInvocation;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -121,7 +122,7 @@ class RepositoryFactorySupportUnitTests {
 		factory.getRepository(ObjectRepository.class);
 
 		verify(listener, times(1)).onCreation(any(MyRepositoryQuery.class));
-		verify(otherListener, times(3)).onCreation(any(RepositoryQuery.class));
+		verify(otherListener, times(4)).onCreation(any(RepositoryQuery.class));
 	}
 
 	@Test // DATACMNS-1538
@@ -253,7 +254,8 @@ class RepositoryFactorySupportUnitTests {
 	@Test // GH-3090
 	void capturesRepositoryMetadata() {
 
-		record Metadata(RepositoryMethodContext context, MethodInvocation methodInvocation) {}
+		record Metadata(RepositoryMethodContext context, MethodInvocation methodInvocation) {
+		}
 
 		when(factory.queryOne.execute(any(Object[].class)))
 				.then(invocation -> new Metadata(RepositoryMethodContextHolder.getContext(),
@@ -298,7 +300,7 @@ class RepositoryFactorySupportUnitTests {
 
 		var repository1 = factory.getRepository(ObjectAndQuerydslRepository.class, backingRepo);
 		var repository2 = factory.getRepository(ObjectAndQuerydslRepository.class, backingRepo);
-		repository1.findByFoo();
+		repository1.findByFoo(null);
 		repository2.deleteAll();
 
 		for (int i = 0; i < 10; i++) {
@@ -428,6 +430,20 @@ class RepositoryFactorySupportUnitTests {
 
 		assertThatThrownBy( //
 				() -> repository.findByClass(null)) //
+				.isInstanceOf(IllegalArgumentException.class) //
+				.hasMessageContaining("must not be null");
+
+	}
+
+	@Test // GH-3100
+	void considersRequiredParameterThroughJspecify() {
+
+		var repository = factory.getRepository(ObjectRepository.class);
+
+		assertThatNoException().isThrownBy(() -> repository.findByFoo(null));
+
+		assertThatThrownBy( //
+				() -> repository.findByNonNullFoo(null)) //
 				.isInstanceOf(IllegalArgumentException.class) //
 				.hasMessageContaining("must not be null");
 	}
@@ -565,8 +581,10 @@ class RepositoryFactorySupportUnitTests {
 		@Nullable
 		Object findByClass(Class<?> clazz);
 
-		@Nullable
-		Object findByFoo();
+		@org.jspecify.annotations.Nullable
+		Object findByFoo(@org.jspecify.annotations.Nullable Object foo);
+
+		Object findByNonNullFoo(@NonNull Object foo);
 
 		@Nullable
 		Object save(Object entity);
