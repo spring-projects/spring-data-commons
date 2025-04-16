c8e16583f7a38f810b0a2594b525aa2c36b6b0d3
Christoph Strobl <christoph.strobl@broadcom.com>
2025-04-16 10:01:27 +0200

    Add LocalVariableNameFactory.
    Add a variable name factory that considers predefined names and resolves name clashes.
    Expose variable name clash resolution via the generation context of a single method.

---
+++ src/main/java/org/springframework/data/repository/aot/generate/LocalVariableNameFactory.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.repository.aot.generate;
+
+import java.util.Set;
+
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * {@link VariableNameFactory} implementation keeping track of defined names resolving name clashes using internal
+ * counter appending {@code _%d} to a suggested name in case of a clash.
+ *
+ * @author Christoph Strobl
+ * @since 4.0
+ */
+class LocalVariableNameFactory implements VariableNameFactory {
+
+	private final MultiValueMap<String, VariableName> variables;
+
+	static LocalVariableNameFactory forMethod(MethodMetadata methodMetadata) {
+		return of(methodMetadata.getMethodArguments().keySet());
+	}
+
+	static LocalVariableNameFactory empty() {
+		return of(Set.of());
+	}
+
+	static LocalVariableNameFactory of(Set<String> variables) {
+		return new LocalVariableNameFactory(variables);
+	}
+
+	LocalVariableNameFactory(Iterable<String> predefinedVariableNames) {
+
+		variables = new LinkedMultiValueMap<>();
+		for (String parameterName : predefinedVariableNames) {
+			variables.add(parameterName, new VariableName(parameterName));
+		}
+	}
+
+	@Override
+	public VariableName generateName(String suggestedName) {
+
+		if (!variables.containsKey(suggestedName)) {
+			VariableName variableName = new VariableName(suggestedName);
+			variables.add(suggestedName, variableName);
+			return variableName;
+		}
+
+		String targetName = suggestTargetName(suggestedName);
+		VariableName variableName = new VariableName(suggestedName, targetName);
+		variables.add(suggestedName, variableName);
+		variables.add(targetName, variableName);
+		return variableName;
+	}
+
+	String suggestTargetName(String suggested) {
+		return suggestTargetName(suggested, 1);
+	}
+
+	String suggestTargetName(String suggested, int counter) {
+
+		String targetName = "%s_%s".formatted(suggested, counter);
+		if (!variables.containsKey(targetName)) {
+			return targetName;
+		}
+		return suggestTargetName(suggested, counter + 1);
+	}
+}
