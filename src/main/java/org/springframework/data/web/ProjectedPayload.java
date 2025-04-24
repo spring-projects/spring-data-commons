39618faf01414f6b166bf905e786b9cae7692ac4
Chris Bono <chris.bono@broadcom.com>
2025-04-24 10:49:46 -0500

    Avoid matching multipart parameters annotated with `@ModelAttribute`
    The ProxyHandlerMethodArgumentResolver now avoids matching multipart parameters annotated with @ModelAttribute. This allows multipart parameters to be handled by RequestParamMethodArgumentResolver which properly handles multipart arguments.
    
    Also, the `@ProjectedPayload` annotation can now be used on parameters. This prepares for the upcoming removal of support for non-annotated projections.
    
    Fixes #3258
    Related tickets #2937
    Original pull request: #3277
    
    Signed-off-by: Chris Bono <chris.bono@broadcom.com>

--- src/main/java/org/springframework/data/web/ProjectedPayload.java
+++ src/main/java/org/springframework/data/web/ProjectedPayload.java
@@ -15,11 +15,10 @@
  */
 package org.springframework.data.web;
 
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
-
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -27,11 +26,12 @@ import java.lang.annotation.Target;
  * response payloads to.
  *
  * @author Oliver Gierke
+ * @author Chris Bono
  * @soundtrack
  * @since 1.13
  */
 @Documented
-@Retention(RUNTIME)
-@Target(TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.PARAMETER })
 public @interface ProjectedPayload {
 }
