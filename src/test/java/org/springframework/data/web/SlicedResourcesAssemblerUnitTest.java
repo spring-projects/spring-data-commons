75b2a88ba836faad890c87e3e459feff991bbc70
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2026-02-03 11:01:06 +0700

    Remove `Assert#notNull` for nullable Link
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- src/test/java/org/springframework/data/web/SlicedResourcesAssemblerUnitTest.java
+++ src/test/java/org/springframework/data/web/SlicedResourcesAssemblerUnitTest.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.AbstractPageRequest;
@@ -257,6 +258,32 @@ class SlicedResourcesAssemblerUnitTest {
 
 		assertThat(model.getRequiredLink(IanaLinkRelations.FIRST).getHref())
 				.isEqualTo("http://localhost/sample?foo=bar&page=0&size=1");
+	}
+
+	@Test // GH-3452
+	void emptySliceCreatorNullLink() {
+
+		var result = assembler.toEmptyModel(EMPTY_SLICE, Person.class, (Link) null);
+
+		var content = result.getContent();
+		assertThat(content).hasSize(1);
+
+		var element = content.iterator().next();
+		assertThat(element).isInstanceOf(EmbeddedWrapper.class);
+		assertThat(((EmbeddedWrapper) element).getRelTargetType()).isEqualTo(Person.class);
+	}
+
+	@Test // GH-3452
+	@SuppressWarnings("deprecation")
+	void emptySliceCreatorRejectsNullOptionalLink() {
+
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> assembler.toEmptyModel(EMPTY_SLICE, Person.class, (Optional<Link>) null));
+	}
+
+	@Test // GH-3452
+	void createsSliceRejectsNullLink() {
+		assertThatIllegalArgumentException().isThrownBy(() -> assembler.toModel(createSlice(1), (Link) null));
 	}
 
 	private static Slice<Person> createSlice(int index) {
